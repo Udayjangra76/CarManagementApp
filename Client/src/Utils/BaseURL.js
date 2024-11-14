1f781acb85b826adfb57a6edbdefe6a8ce16a038
Uday Jangra <uday.jangra76@gmail.com>
2024-11-15 03:04:51 +0530

    backend url added2

--- Client/src/Utils/BaseURL.js
+++ Client/src/Utils/BaseURL.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://car-management-app-one.vercel.app",
     withCredentials: true,
 });
 
